a62299d4d5a5ef7b1786c554916428604cd195f4
Long Nguyen <nguyelongt@gmail.com>
2017-10-28 02:34:44 -0400

    Added Status and Tweaked PurchaseRequests

---
+++ PRSWebApplication/PRSWebLibrary/Models/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRSWebLibrary.Models
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
